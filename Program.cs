d62b64c727051c2442ccb564f9307dcea4f37a93
Wagner <wagner_silvajr@hotmail.com>
2024-04-19 15:36:47 -0300

    verificação de argumentos e parametros

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-﻿string[] guestList = {"Rebecca", "Nadia", "Noor", "Jonte"};
+﻿string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
 string[] rsvps = new string[10];
 int count = 0;
 
-void RSVP(string name, int partySize, string allergies, bool inviteOnly) 
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+RSVP("Noor", 4, inviteOnly: false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
 {
     if (inviteOnly)
     {
-        // search guestList before adding rsvp
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
     }
 
     rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
